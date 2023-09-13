6055185bf32df4428a6dbac8de77d099fe5d78b4
JuanRamos15 <124464419+JuanRamos15@users.noreply.github.com>
2023-09-13 20:50:50 +0000

    🧪 S02A01 Webpack Webpack en front-end y babel para el frontend

--- .webpack.config.js
+++ .webpack.config.js
@@ -11,9 +11,46 @@ module.exports = {
         path: path.resolve(__dirname,"public"),
         //2.2 nombre de salida
         filename: "bundle.js",
-        //2.3 Ruta base de archivos
-        publicPath: "/"
+  
     },
+    output: {
+        // 2.1 Absolute output path
+        // Note that it is being placed in the directory
+        // of the project's static files
+        path: path.resolve(__dirname, "public"),
+        // 2.2 Output file name
+        filename: "bundle.js"
+      },
+      // Agregando un modulo a webpack
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: [
+              {
+                loader: 'babel-loader',
+                options: {
+                  presets: [
+                    [
+                      '@babel/preset-env',
+                      {
+                        'modules': false,
+                        'useBuiltIns': 'usage',
+                        'targets': '> 0.25%, not dead',
+                        'corejs': 3
+                      }
+                    ]
+                  ]
+                }
+              }
+            ]
+          }
+        ]
+      }
+    }
+    
+    /*
     //3. servidor de desarollo
     devServer: {
         //3.1 Folder de estaticos
@@ -24,3 +61,4 @@ module.exports = {
         host: '0.0.0.0'
     }
 }
+*/
