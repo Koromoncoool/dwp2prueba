e9ede990b8fae53ca94cf1407f39c4939815f719
JuanRamos15 <124464419+JuanRamos15@users.noreply.github.com>
2023-10-09 16:07:25 +0000

    🧪 S02A06 Materialize CSS: Se instalo la dependencia de Materialize y se cargan los estilos

--- client/index.js
+++ client/index.js
@@ -1,5 +1,13 @@
 // cargando estilos
+// eslint-disable-next-line no-console
 import './styles/style.css';
 // Mensaje en la Consola
-// eslint-disable-next-line no-console
+/* eslint-diable */
+// importando estilos de materialize
+import 'materialize-css/dist/css/materialize.css';
+// importando estilos de de materialize
+import 'materialize-css/dist/js/materialize';
+/* eslint-enable */
+// inicia scripts de materialize para interactividad
+M.AutoInit();
 console.log('🎉Estilos cargado Correctamente🎉');
